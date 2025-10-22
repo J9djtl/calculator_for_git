8acc016b008695a67bc772fdc72ac69a7bb15641
Nechaeva Sofia <sonyarepman0803@gmail.com>
2025-10-22 09:19:39 +0300

    Clear front draft with docstring

--- frontend.py
+++ frontend.py
@@ -1,7 +1,6 @@
 from tkinter import *
 from tkinter import ttk
-from logic import evaluate_expression, apply_operation
-
+from logic import evaluate_expression
 
 class CalculatorGUI:
     def __init__(self, root):
@@ -12,7 +11,7 @@ class CalculatorGUI:
         self.frame_top.pack(pady=10)
 
         self.label = ttk.Label(self.frame_top, text="Это наш калькулятор")
-        self.label.pack(anchor='e')
+        self.label.pack(anchor='ne')
 
         self.entry = ttk.Entry(self.frame_top)
         self.entry.pack(anchor='center', padx=8, pady=8)
@@ -30,7 +29,7 @@ class CalculatorGUI:
 
         self.render_buttons()
 
-    def on_click(self, char):
+    def on_button_click(self, char):
         current = self.entry.get()
         self.entry.delete(0, END)
         self.entry.insert(0, current + char)
@@ -38,7 +37,7 @@ class CalculatorGUI:
     def on_equal_press(self):
         expression = self.entry.get()
         try:
-            result = str(eval(expression))  # временно
+            result = str(eval(expression))  # временно, сюда evaluate_expression()
         except Exception:
             result = "Ошибка"
         self.entry.delete(0, END)
@@ -57,7 +56,7 @@ class CalculatorGUI:
             elif char == 'C':
                 cmd = self.on_clear
             else:
-                cmd = lambda t=char: self.on_click(t)
+                cmd = lambda t=char: self.on_button_click(t)
 
             btn = ttk.Button(self.frame_buttons, text=char, command=cmd)
             btn.grid(row=row, column=col, padx=5, pady=5)
