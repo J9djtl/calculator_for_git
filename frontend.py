976d1468dbe448475018827df5716f5ea4846914
Nechaeva Sofia <sonyarepman0803@gmail.com>
2025-10-29 15:21:05 +0300

    All buttons with dumb ifs and without style

--- frontend.py
+++ frontend.py
@@ -5,7 +5,7 @@ from logic import evaluate_expression
 class CalculatorGUI:
     def __init__(self, root):
         self.root = root
-        self.n_cols = 4
+
 
         self.frame_top = ttk.Frame(root)
         self.frame_top.pack(pady=10)
@@ -19,13 +19,16 @@ class CalculatorGUI:
         self.frame_buttons = ttk.Frame(root)
         self.frame_buttons.pack(pady=10)
 
+        self.n_cols = 6
         self.buttons = [
-            '', '', '', 'C',
-            '7', '8', '9', '/',
-            '4', '5', '6', '*',
-            '1', '2', '3', '-',
-            '.', '0', '=', '+',
+            'MC', 'MR', 'MS', 'M+', 'M−', 'AС',       
+            '(', ')', 'C', '⌫', '%',  'ANS',           
+            '7', '8', '9', '/', 'sqrt', 'sin',          
+            '4', '5', '6', '*', '^', 'cos',         
+            '1', '2', '3', '-', '1/x', 'floor',           
+            '.', '0', '=', '+', '10ˣ', 'ceil'           
         ]
+
 
         self.render_buttons()
 
@@ -46,6 +49,31 @@ class CalculatorGUI:
     def on_clear(self):
         self.entry.delete(0, END)
 
+    def on_backspace(self):
+        current = self.entry.get()
+        self.entry.delete(0, END)
+        self.entry.insert(0, current[:-1])
+
+    def on_memory_stub(self):
+        print("Функции памяти пока не реализованы")
+
+    def on_all_clear(self):
+        self.entry.delete(0, END)
+        # Здесь можно добавить сброс памяти, истории и т.д.
+
+    def on_insert_function(self, func_name):
+        current = self.entry.get()
+        self.entry.delete(0, END)
+        self.entry.insert(0, current + func_name + '(')
+
+    def on_insert_special(self, pattern):
+        current = self.entry.get()
+        self.entry.delete(0, END)
+        self.entry.insert(0, current + pattern)
+
+
+
+
     def render_buttons(self):
         for index, char in enumerate(self.buttons):
             row = index // self.n_cols
@@ -55,6 +83,20 @@ class CalculatorGUI:
                 cmd = self.on_equal_press
             elif char == 'C':
                 cmd = self.on_clear
+            elif char == '⌫':
+                cmd = self.on_backspace
+            elif char == 'AC':
+                cmd = self.on_all_clear
+            elif char == '1/x':
+                cmd = lambda: self.on_button_click('1/(')
+            elif char == '10ˣ':
+                cmd = lambda: self.on_button_click('10^(')
+            elif char in {'sqrt', 'sin', 'cos', 'floor', 'ceil'}:
+                cmd = lambda t=char: self.on_button_click(t + '(')
+            elif char in {'MC', 'MR', 'MS', 'M+', 'M−', 'ANS'}:
+                cmd = self.on_memory_stub
+            elif char == '':
+                pass  # пропускаем пустую кнопку
             else:
                 cmd = lambda t=char: self.on_button_click(t)
 
